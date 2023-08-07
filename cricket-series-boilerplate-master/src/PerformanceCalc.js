2c7f1cb42e070c5cd19b7bc0a4cb1ce10e5fd7be
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-07 13:22:43 +0500

    Cricket Series Done

--- cricket-series-boilerplate-master/src/PerformanceCalc.js
+++ cricket-series-boilerplate-master/src/PerformanceCalc.js
@@ -1,6 +1,23 @@
 
 const PerformanceCalculator=(runs,balls)=>{
       // Write the Logic here
+      let average=runs/balls;
+      if (runs>30 && balls<15) {
+        average=average+(average*0.01);
+            
+      }
+      if (runs>50 && balls<25) {
+            average=average+(average*0.10);
+                
+      }
+
+      if (runs>100 && balls<50) {
+            average=average+(average*0.20);
+                
+      }
+
+        return average.toFixed(2);  
+     
 }
 
 module.exports={PerformanceCalculator}
