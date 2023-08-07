21b2847dbacce43eb51f34a6f97da775189b889c
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-07 12:43:07 +0500

    Banking Application Done

--- banking-application-boilerplate-master/src/customer.js
+++ banking-application-boilerplate-master/src/customer.js
@@ -5,20 +5,13 @@ var customerList=[];
 const addCustomer=(CustomerId, CustomerName,CustomerAge,CustomerAddress,CustomerContactNumber,Category)=>{
       let notAdded=customerList.find(customer=>customer[0]==CustomerId);
       if (!notAdded) {
-            // customerList.push({Id:CustomerId,name:CustomerName,age:CustomerAge,
-            // address:CustomerAddress,contact:CustomerContactNumber,category:Category});
             customerList.push([CustomerId, CustomerName,CustomerAge,CustomerAddress,CustomerContactNumber,Category])
       }
 }
 
 const updateCustomer=(CustomerId, CustomerName,CustomerAge,CustomerAddress,CustomerContactNumber,Category)=>{
-      let customer=customerList.find(customer=>customer[0]==CustomerId);
+      let customer = customerList.find(customer=>customer[0]==CustomerId);
       if (customer) {
-      //      customer.name=CustomerName;
-      //      customer.age=CustomerAge;
-      //      customer.address=CustomerAddress;
-      //      customer.contact=CustomerContactNumber;
-      //      customer.Category=Category;
            customer[1]=CustomerName;
            customer[2]=CustomerAge;
            customer[3]=CustomerAddress;
