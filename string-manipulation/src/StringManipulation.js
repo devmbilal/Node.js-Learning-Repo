59206113720e559ad17d5f4ca63c4a73ccaf2a88
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-08 13:30:56 +0500

    String Manipulation Added

---
+++ string-manipulation/src/StringManipulation.js
@@ -0,0 +1,38 @@
+//count the number of Characters in the given String and return the value
+const countCharacters = (str) => {
+  
+}
+//count the number of vowels in the given String and return the value
+const countVowels = (str) => {
+ 
+}
+
+//Check the existence of the given String in the Specified String and return the value
+const checkExistenceOfStr = (str, checkStr) => {
+  
+}
+
+//replace a word and return the value
+const replaceWord = (str, wordToBeReplaced, replaceWord) => {
+  //return str.replace(wordToBeReplaced, replaceWord);
+}
+
+//convert the specified string into Title Case and return the value
+const titleCaseConversion = (str) => {
+  
+}
+
+// find the largest word (in terms of length) in the specified string and return the value
+const findLongestWord = (str) => {
+  
+}
+
+
+module.exports = {
+  countCharacters,
+  countVowels,
+  checkExistenceOfStr,
+  replaceWord,
+  titleCaseConversion,
+  findLongestWord
+}
