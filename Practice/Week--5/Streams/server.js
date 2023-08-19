3acb2b08db8307e9d098bf8d22f5244ff958c0d8
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-19 19:44:03 +0500

    Godd way copy file added

--- Practice/Week--5/Streams/server.js
+++ Practice/Week--5/Streams/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const { log } = require('console');
 
 const server = http.createServer((req,res) => {
  
@@ -7,18 +8,29 @@ const server = http.createServer((req,res) => {
       return res.end('404 not found');
     }
      
-    // Downloading big files in bad way
+  // Downloading big files in bad way
 
     //const file = fs.readFileSync('sample.txt'); 
     //return res.end(file);
 
-    // Downloading big files in Good way
+  // Downloading big files in Good way
  
-    const readableStream = fs.createReadStream('sample.txt');
-    
+    // const readableStream = fs.createReadStream('sample.txt');
     // readable stream pipe to writeable straeam
+    // readableStream.pipe(res);
 
-    readableStream.pipe(res);
+  // Copying big files in Good way
+
+    // const readStream = fs.createReadStream('sample.txt');
+    // const writeStream = fs.createWriteStream('output.txt');
+    // readStream.on('data', (chunk) => {
+    //   console.log('Chunk',chunk.toString());
+    //   writeStream.write(chunk); 
+    // });
+    // res.end();
+  
+  
+    
    
 
 });
