2654bf39e19ba57c8dbb282ccd46670f86531a85
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-22 22:47:36 +0500

    input validation added

--- APIS/restful-apis/index.js
+++ APIS/restful-apis/index.js
@@ -26,7 +26,12 @@ app.get('/api/courses/:id',(req, res) =>{
     res.send(course);
 });
 
-app.post('/api/courses', (req, res) => {    
+app.post('/api/courses', (req, res) => {   
+    if(!req.body.name || req.body.name.length < 3){
+        //400 Bad Request
+        res.status(400).send('Name is required and should be minimum 3 characters');
+        return;
+    }
     const course = {
         id:courses.length+1,
         name:req.body.name
