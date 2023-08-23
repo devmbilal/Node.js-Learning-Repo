1910959f99a09f7911a9f53ea75cf7801bef67b4
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-23 10:41:24 +0500

    built In Middleware added

--- APIS/restful-apis/index.js
+++ APIS/restful-apis/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const Joi = require('joi');
 const logger = require('./logger');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use(logger);
+app.use(helmet());
+app.use(morgan('tiny'));
 
 const courses = [
     {id: 1, name: 'course1'},   
