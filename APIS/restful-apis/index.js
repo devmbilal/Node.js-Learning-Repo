7cb84ef410cbc77f452a38f3f20f3aabc94bb5c1
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-23 10:50:30 +0500

    Use Morgan on Development Environment

--- APIS/restful-apis/index.js
+++ APIS/restful-apis/index.js
@@ -5,15 +5,18 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const app = express();
 
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`);   
-console.log(`app: ${app.get('env')}`);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use(logger);
 app.use(helmet());
-app.use(morgan('tiny'));
+
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...');
+}
+
 
 const courses = [
     {id: 1, name: 'course1'},   
