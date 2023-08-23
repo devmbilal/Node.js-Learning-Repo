e7abbccc9f95b16019c2de0fde56dea66533e61f
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-23 10:36:32 +0500

    static middleware added

--- APIS/restful-apis/index.js
+++ APIS/restful-apis/index.js
@@ -4,6 +4,7 @@ const logger = require('./logger');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
 app.use(logger);
 
 const courses = [
