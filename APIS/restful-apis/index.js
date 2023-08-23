0c1565f71f5ba03893e9aba74f00be74cc069c22
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-23 10:34:28 +0500

    urlencoded middleware added

--- APIS/restful-apis/index.js
+++ APIS/restful-apis/index.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const logger = require('./logger');
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(logger);
 
 const courses = [
