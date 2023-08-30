2b5f14e259103fc37da505c9bd2489eaf14a16a4
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-30 16:51:20 +0500

    Schema added

--- Express/Database/index.js
+++ Express/Database/index.js
@@ -2,4 +2,12 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
   .then(()=>console.log('Connected to MongoDB...'))
-  .catch(err=>console.log('Could not connect to MongoDB...',err))
+  .catch(err=>console.log('Could not connect to MongoDB...',err)) 
+
+const courseSchema = new mongoose.Schema({
+    name:String,
+    author:String,
+    tags:[String],
+    date:{type:Date,default:Date.now},
+    isPublished:Boolean
+});
