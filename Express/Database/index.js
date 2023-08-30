f061e791f95ce9484d96adee2b8a90986d8ffbee
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-30 17:16:28 +0500

    models and object added

--- Express/Database/index.js
+++ Express/Database/index.js
@@ -11,3 +11,11 @@ const courseSchema = new mongoose.Schema({
     date:{type:Date,default:Date.now},
     isPublished:Boolean
 });
+
+const Course=mongoose.model('Course',courseSchema);
+const course = new Course({
+    name:'Node.js Course',
+    author:'Muhammad Bilal',
+    tags:['node','backend'],
+    isPublished:true
+})
