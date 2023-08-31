e40ae4b2ce5444a208750a44c38512c3932dc51c
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-31 12:09:55 +0500

    getCourse Implemented

--- Express/Database/index.js
+++ Express/Database/index.js
@@ -12,12 +12,13 @@ const courseSchema = new mongoose.Schema({
     isPublished:Boolean
 });
 
+const Course=mongoose.model('Course',courseSchema);
+
 async function createCourse(){
-    const Course=mongoose.model('Course',courseSchema);
     const course = new Course({
     name:'Node.js Course',
     author:'Muhammad Bilal',
-    tags:['angular','fronten'],
+    tags:['angular','frontend'],
     isPublished:true
 })
    const result = await course.save();
@@ -25,5 +26,14 @@ async function createCourse(){
 }
 
 createCourse();
+async function getCourses(){
+   const courses = await Course 
+    .find({author:'Muhammad Bilal',isPublished:true})
+    .limit(10)
+    .sort({name:1})
+    .select({name:1,tags:1});
+    console.log(courses);
+}
 
 
+getCourses();
